3146024d85aadabf5a966ee16c8dff0afc6f81cd
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-06-20 14:36:10 +0530

    fifth commit

--- src/streams/MainClass.java
+++ src/streams/MainClass.java
@@ -1,6 +1,5 @@
 package streams;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -8,6 +7,9 @@ import java.util.stream.Stream;
 public class MainClass {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(12, 23, 26, 2, 8, 4, 10, 100, 999);
+
+//        //Getting A Stream:
+//        Stream<Integer> stream = numbers.stream();
 
 
         //filter(Predicate) + collect():
@@ -36,5 +38,28 @@ public class MainClass {
         //map() + sorted() + forEach():
         System.out.println("Converting Names Into UpperCase:");
         names.stream().map(name -> name.toUpperCase()).sorted().forEach(System.out::println);
+
+        //flatMap() + foEach():
+        System.out.println("FlatMap Example:");
+        names.stream().flatMap(name -> Stream.of(name.charAt(2))).forEach(System.out::println);
+
+        //Extra Methods:
+
+        //min():
+        Integer min = numbers.stream().min((x, y) -> x.compareTo(y)).get();
+        System.out.println("Minimum Number In List Is: " + min);
+
+        //max():
+        Integer max = numbers.stream().max((x, y) -> x.compareTo(y)).get();
+        System.out.println("Maximum Number In List Is: " + max);
+
+        //count():
+        long count = numbers.stream().count();
+        System.out.println("There Are " + count + " Numbers In List");
+
+        //distinct():
+        List<Integer> integers = List.of(1,1,1,2,2,3);
+        long distinctCount = integers.stream().distinct().count();
+        System.out.println("There Are " + distinctCount + " Distinct Numbers In List");
     }
 }
